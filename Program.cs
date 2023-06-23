21402e28c31d7133186cfbf0ed7a26c65d2fdd49
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-23 11:47:14 -0400

    Wrote out 15 repititions of for loops

--- Program.cs
+++ Program.cs
@@ -1,3 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+//1
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//2
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//3
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//4
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//5
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//6
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//7
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//8
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//9
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//10
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//11
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//12
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//13
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//14
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//15
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
